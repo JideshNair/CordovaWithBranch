44797179fdfb2572f3d40264f607ed0e6dfe0855
JideshNair <jidesh@clevertap.com>
2020-06-15 17:03:46 +0530

    Initial commit

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "clevertap-cordova.CleverTap",
+      "file": "plugins/clevertap-cordova/www/CleverTap.js",
+      "pluginId": "clevertap-cordova",
+      "clobbers": [
+        "CleverTap"
+      ]
+    },
+    {
+      "id": "branch-cordova-sdk.Branch",
+      "file": "plugins/branch-cordova-sdk/src/index.js",
+      "pluginId": "branch-cordova-sdk",
+      "clobbers": [
+        "Branch"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4",
+    "clevertap-cordova": "2.2.0",
+    "branch-cordova-sdk": "4.1.2"
+  };
+});
