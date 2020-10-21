ef56b664e4358cd502bcf9cb29a571e31fc210d2
JideshNair <jidesh@clevertap.com>
2020-10-21 11:48:17 +0530

    ios

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,15 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "branch-cordova-sdk.Branch",
+      "file": "plugins/branch-cordova-sdk/src/index.js",
+      "pluginId": "branch-cordova-sdk",
+      "clobbers": [
+        "Branch"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "branch-cordova-sdk": "4.1.2"
+  };
+});
